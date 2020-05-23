d3e4eec4be80dc0ad4faafcd8b4c38982729a35f
KrzysztofIwaszkiewicz <krz.iwaszkiewicz@gmail.com>
2020-05-23 14:59:17 +0200

    navigation style added to mainverbs.html + added class selector to main element(text centered)

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const firstName = "Krzysiek";
 const age = 35;
 
 console.log(`¡Hola! Mam na imię ${firstName} i mam ${age} lat.`);
+
+const heading = document.querySelector('.main-heading--js');
+
+heading.innerHTML = `¡Hola! Mam na imię ${firstName} i mam ${age} lat.`
